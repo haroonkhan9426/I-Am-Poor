575338f3b8d624baa8d03b653a53469bc6bcc6f7
Haroon khan <haroonkhan9426@gmail.com>
2019-08-26 23:36:03 +0500

    Text styles added to App bar text and app finalized

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(IamPoor());
 
-class IamPoor extends StatelessWidget{
+class IamPoor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: _renderMainScreen(),
     );
   }
-  
-  Widget _renderMainScreen(){
+
+  Widget _renderMainScreen() {
     return Scaffold(
       appBar: AppBar(
-        title: Text('I am Poor'),
+        title: Center(child: Text('I am Poor',
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.white
+        ),),),
         backgroundColor: Colors.green[800],
       ),
       body: _renderBody(),
     );
   }
 
-  Widget _renderBody(){
+  Widget _renderBody() {
     return Center(
       child: Image(
-        image: AssetImage(''),
+        image: AssetImage('assets/images/art.png'),
       ),
     );
   }
-  
 }
