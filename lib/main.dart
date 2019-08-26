c4e879f0df496d492d950502359d619c9c90313f
Haroon khan <haroonkhan9426@gmail.com>
2019-08-26 23:05:59 +0500

    Basic widgets structure created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(IamPoor());
+
+class IamPoor extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: _renderMainScreen(),
+    );
+  }
+  
+  Widget _renderMainScreen(){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('I am Poor'),
+        backgroundColor: Colors.green[800],
+      ),
+      body: _renderBody(),
+    );
+  }
+
+  Widget _renderBody(){
+    return Center(
+      child: Image(
+        image: AssetImage(''),
+      ),
+    );
+  }
+  
+}
